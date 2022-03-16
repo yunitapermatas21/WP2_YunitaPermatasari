5941d802c5cb9d3f1ae10ea5d22d71022411f649
Yunita <79780225+yunitapermatas21@users.noreply.github.com>
2022-03-16 13:25:14 +0700

    Add files via upload
    Tugas Web Programming 2 - Pertemuan 1 (17190418)

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Yunita Permatasari, 
+                saya tinggal di daerah Depok.
+                    Olahraga yang saya sukai adalah Bersepeda";
+    }
+}
