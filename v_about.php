1fe5553c9fe47f77a24f9228d5c77cad7952c2f3
Yunita <79780225+yunitapermatas21@users.noreply.github.com>
2022-04-13 11:09:40 +0700

    Add files via upload
    Latihan Pertemuan 4 - WP2

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama Depan : Yunita</li>
+    <li>Nama Belakang : Permatasari</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+    <li> Jalan Mandor Jaun 8 No.44 Pancoranmas, Depok</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+    <li>Depok</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+    <li>Bersepeda</li>
+    <li>Lari</li>
+ </ul>
+</section>
